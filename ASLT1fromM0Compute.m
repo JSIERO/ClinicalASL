d354dad65c699094e2eebd09c835d419b1c77de4
JSIERO <76999501+JSIERO@users.noreply.github.com>
2023-07-14 12:25:26 +0200

    Update ASLT1fromM0Compute.m
    added more explaination

--- ASLT1fromM0Compute.m
+++ ASLT1fromM0Compute.m
@@ -7,23 +7,24 @@ function T1fromM0=ASLT1fromM0Compute(DATA4D, MASK, TIMEARRAY)
 dims = size(DATA4D);
 DATA2D = reshape(DATA4D, dims(1)*dims(2)*dims(3),dims(4));
 
-%take natural log of data
-DATA2D = log(DATA2D);
+%take the natural log of data
+DATA2D = log(DATA2D);, and mask to include brain only
 
 CONSTANTARRAY = ones(size(DATA2D, 2), 1); % create array with ones, for the constant term
 T1fit = zeros(size(DATA2D,1),2);
 warning off
 for i = 1:size(DATA2D,1)
-A = [CONSTANTARRAY, DATA2D(i,:)'];
-T1fit(i,:) = A\TIMEARRAY';
+A = [CONSTANTARRAY, DATA2D(i,:)']; % construct system of linear equations: Ax=b
+T1fit(i,:) = A\TIMEARRAY'; % solve the system of linear equations, yielding the decay rate constant (R1 weighted) and offset (not used)
 end
 warning on
 
-data_T1fit2 = reshape(T1fit(:,2),dims(1),dims(2),dims(3));
-data_T1fit2_brain = -1/data_T1fit2.*MASK*1e3;
-data_T1fit2_brain(isnan(data_T1fit2_brain)) = 0;
-data_T1fit2_brain_mask_high = data_T1fit2_brain<=300; % mask to exlude too high values after fit 
-data_T1fit2_brain_mask_low = data_T1fit2_brain>0; % mask to exlude negative values after fit (CSF)
+data_R1fit = reshape(T1fit(:,2),dims(1),dims(2),dims(3)); % reshape to construct a 3D map of the decay rate constant from the linear fit
+data_T1fit_brain = -1/data_R1fit.*MASK*1e3; % convert decay rate constant to the decay time constant (T1 weighted) and impose mask to include brain-only voxels
+data_T1fit_brain(isnan(data_T1fit_brain)) = 0;
 
-T1fromM0 = data_T1fit2_brain.*data_T1fit2_brain_mask_high.*data_T1fit2_brain_mask_low;
+data_T1fit_brain_mask_high = data_T1fit_brain<=300; % mask to exclude too high values after fit 
+data_T1fit_brain_mask_low = data_T1fit_brain>0; % mask to exclude negative values after fit (e.g. CSF)
+
+T1fromM0 = data_T1fit_brain.*data_T1fit_brain_mask_high.*data_T1fit_brain_mask_low;
 T1fromM0(isnan(T1fromM0)) = 0;
