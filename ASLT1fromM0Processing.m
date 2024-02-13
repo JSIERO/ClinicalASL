ed3b56a248f9aaccf8e01543d963814f42934fc8
jeroen <jeroen@ein.infiniband>
2024-02-13 10:07:15 +0100

    replaced 'eval' with 'system' to execute cmdline

--- ASLT1fromM0Processing.m
+++ ASLT1fromM0Processing.m
@@ -3,10 +3,10 @@ function SUBJECT = ASLT1fromM0Processing(SUBJECT, prefix)
 % Construct T1w image from multi PLD M0 image
 
 % brain extraction on M0 image
-eval(['!bet ' SUBJECT.ASLdir prefix '_M0.nii.gz  ' SUBJECT.ASLdir prefix '_M0_brain.nii.gz' ' -m -f 0.4 -g 0']) % extract brain mask of M0's
-eval(['!fslmaths ' SUBJECT.ASLdir prefix '_M0_brain_mask -dilM ' SUBJECT.ASLdir prefix '_temp_M0_brain_mask_dilM']);
-eval(['!fslmaths ' SUBJECT.ASLdir prefix '_M0_brain_mask -kernel 2D -ero ' SUBJECT.ASLdir prefix '_temp_M0_brain_mask_ero']);
-eval(['!fslmaths ' SUBJECT.ASLdir prefix '_M0 -mul ' SUBJECT.ASLdir prefix '_temp_M0_brain_mask_ero -kernel 2D -dilD ' SUBJECT.ASLdir prefix '_temp_M0_dilD']);
+system(['bet ' SUBJECT.ASLdir prefix '_M0.nii.gz  ' SUBJECT.ASLdir prefix '_M0_brain.nii.gz' ' -m -f 0.4 -g 0']) % extract brain mask of M0's
+system(['fslmaths ' SUBJECT.ASLdir prefix '_M0_brain_mask -dilM ' SUBJECT.ASLdir prefix '_temp_M0_brain_mask_dilM']);
+system(['fslmaths ' SUBJECT.ASLdir prefix '_M0_brain_mask -kernel 2D -ero ' SUBJECT.ASLdir prefix '_temp_M0_brain_mask_ero']);
+system(['fslmaths ' SUBJECT.ASLdir prefix '_M0 -mul ' SUBJECT.ASLdir prefix '_temp_M0_brain_mask_ero -kernel 2D -dilD ' SUBJECT.ASLdir prefix '_temp_M0_dilD']);
 
 NII = load_untouch_nii([SUBJECT.ASLdir prefix '_M0_brain_mask.nii.gz']);
 SUBJECT.(prefix).brainmask = logical(NII.img);
@@ -21,7 +21,7 @@ M0_dilD = double(NII.img);
 SUBJECT.(prefix).M0_forQCBF = ASLSmoothImage(M0_dilD, 3, SUBJECT.FWHM_M0, SUBJECT.VOXELSIZE);
 
 SaveDataNII(SUBJECT.(prefix).M0_forQCBF , [SUBJECT.ASLdir prefix '_M0_forQCBF.nii.gz'], SUBJECT.dummyfilenameSaveNII, 1, [0 500],SUBJECT.TR) % save T1fromM0
-eval(['!rm ' SUBJECT.ASLdir '*temp*']); % remove temp files
+system(['rm ' SUBJECT.ASLdir '*temp*']); % remove temp files
 
 % compute T1w image from multi-delay PCASL (Look-Locker) M0 data across the different PLDs
 for i = 1:SUBJECT.NPLDS
@@ -33,8 +33,8 @@ T1fromM0 = ASLT1fromM0Compute(M0_allPLD_noLLcorr, SUBJECT.(prefix).brainmask, SU
 SaveDataNII(T1fromM0, [SUBJECT.ASLdir prefix '_T1fromM0.nii.gz'], SUBJECT.dummyfilenameSaveNII, 1, [0 500],SUBJECT.TR) % save T1fromM0
 
 % tissue segment T1fromM0 using FSL FAST and load into SUBEJCT struct
-eval(['!fast -b -g -B ' [SUBJECT.ASLdir prefix '_T1fromM0.nii.gz'] ' ' [SUBJECT.ASLdir prefix '_T1fromM0.nii.gz']]) 
-eval(['!fslmaths ' SUBJECT.ASLdir prefix '_T1fromM0_restore.nii.gz ' SUBJECT.ASLdir prefix '_T1fromM0.nii.gz']); % use the _restore data as the T1fromM0
+system(['fast -b -g -B ' [SUBJECT.ASLdir prefix '_T1fromM0.nii.gz'] ' ' [SUBJECT.ASLdir prefix '_T1fromM0.nii.gz']]) 
+system(['fslmaths ' SUBJECT.ASLdir prefix '_T1fromM0_restore.nii.gz ' SUBJECT.ASLdir prefix '_T1fromM0.nii.gz']); % use the _restore data as the T1fromM0
 
 NII = load_untouch_nii([SUBJECT.ASLdir prefix '_T1fromM0.nii.gz']);
 SUBJECT.(prefix).T1fromM0 = double(NII.img);
