205fc2ac14c39efab8a7909754745f3450cd9c88
jeroen <jeroen@blue.infiniband>
2025-04-17 14:49:57 +0200

    added a fast processing script(s) for MRI DIAMOX

--- ASLPrepareASLDataDICOM.m
+++ ASLPrepareASLDataDICOM.m
@@ -1,4 +1,4 @@
-function SUBJECT = ASLPrepareASLDataDICOM(SUBJECT, filename, prefix)
+function SUBJECT = ASLPrepareASLDataDICOM(SUBJECT, filename, prefix , fast)
 % ClinicalASL toolbox 2023, JCWSiero
 % Prepare (multidelay) ASL data: interleave control and label files per PLD, M0 and perform Look Locker Correction
 
@@ -12,13 +12,13 @@ SUBJECT.(prefix).ASL_label1label2_allPLD = zeros(DIMS(1),DIMS(2), DIMS(3),SUBJEC
 for i = 1:SUBJECT.NPLDS
     SUBJECT.(prefix).M0ASL_allPLD(:,:,:,1:SUBJECT.NDYNS,i,1) = DATA(:,:,:,i:2*SUBJECT.NPLDS:SUBJECT.NPLDS*SUBJECT.NDYNS*2)./ SUBJECT.LookLocker_correction_factor_perPLD(i);% for label images per PLD per volume
     SUBJECT.(prefix).M0ASL_allPLD(:,:,:,1:SUBJECT.NDYNS,i,2) = DATA(:,:,:,i+SUBJECT.NPLDS:2*SUBJECT.NPLDS:SUBJECT.NPLDS*SUBJECT.NDYNS*2)./ SUBJECT.LookLocker_correction_factor_perPLD(i);% for control images per PLD per volume
-   
+
     SaveDataNII(squeeze(SUBJECT.(prefix).M0ASL_allPLD(:,:,:,:,i,1)), [SUBJECT.ASLdir prefix '_PLD0' num2str(i) '_label1'], SUBJECT.dummyfilenameSaveNII, 1, [], SUBJECT.TR);
     SaveDataNII(squeeze(SUBJECT.(prefix).M0ASL_allPLD(:,:,:,:,i,2)), [SUBJECT.ASLdir prefix '_PLD0' num2str(i) '_label2'], SUBJECT.dummyfilenameSaveNII, 1, [], SUBJECT.TR);
 end
 
 % Interleave control and label ASL data per PLD, M0 and perform Look Locker Correction
-for i = 1:SUBJECT.NPLDS   
+for i = 1:SUBJECT.NPLDS
     SUBJECT.(prefix).ASL_label1label2_allPLD(:,:,:,:,i) = ASLInterleaveControlTag(squeeze(SUBJECT.(prefix).M0ASL_allPLD(:,:,:,2:end,i,1)), squeeze(SUBJECT.(prefix).M0ASL_allPLD(:,:,:,2:end,i,2)));
     SaveDataNII(squeeze(SUBJECT.(prefix).ASL_label1label2_allPLD(:,:,:,:,i)), [SUBJECT.ASLdir prefix '_PLD0' num2str(i) '_label1label2'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save M0 image , only first PLDs
 end
@@ -26,15 +26,22 @@ end
 disp('Saving ASL data interleaved label control: all PLDs')
 SaveDataNII(reshape(SUBJECT.(prefix).ASL_label1label2_allPLD, DIMS(1),DIMS(2), DIMS(3), SUBJECT.NPLDS*SUBJECT.NREPEATS*2), [SUBJECT.ASLdir prefix '_allPLD_label1label2'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save interleaved control label and for all PLDs as 4th dimension
 
-disp('Saving ASL data interleaved label control: 1-to-2 PLDs    for ATA, aCBF maps, and spatial COV ')
-SaveDataNII(reshape(SUBJECT.(prefix).ASL_label1label2_allPLD(:,:,:,:,2:end), DIMS(1),DIMS(2), DIMS(3), (SUBJECT.NPLDS-1)*SUBJECT.NREPEATS*2), [SUBJECT.ASLdir prefix '_2tolastPLD_label1label2'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save interleaved control label and for all PLDs as 4th dimension
-
+if strcmp(fast, 'fast')
+    % skip saving the 1-to-2 PLDs for ATA, aCBF maps, and spatial COV
+else
+    disp('Saving ASL data interleaved label control: 1-to-2 PLDs    for ATA, aCBF maps, and spatial COV ')
+    SaveDataNII(reshape(SUBJECT.(prefix).ASL_label1label2_allPLD(:,:,:,:,1:2), DIMS(1),DIMS(2), DIMS(3), 2*SUBJECT.NREPEATS*2), [SUBJECT.ASLdir prefix '_1to2PLD_label1label2'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save interleaved control label and for all PLDs as 4th dimension
+end
 disp('Saving ASL data interleaved label control: 2-to-last PLDs for CBF maps, "free" of arterial transit artefacts')
-SaveDataNII(reshape(SUBJECT.(prefix).ASL_label1label2_allPLD(:,:,:,:,1:2), DIMS(1),DIMS(2), DIMS(3), 2*SUBJECT.NREPEATS*2), [SUBJECT.ASLdir prefix '_1to2PLD_label1label2'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save interleaved control label and for all PLDs as 4th dimension
+SaveDataNII(reshape(SUBJECT.(prefix).ASL_label1label2_allPLD(:,:,:,:,2:end), DIMS(1),DIMS(2), DIMS(3), (SUBJECT.NPLDS-1)*SUBJECT.NREPEATS*2), [SUBJECT.ASLdir prefix '_2tolastPLD_label1label2'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save interleaved control label and for all PLDs as 4th dimension
 
 % Construct M0 calibration image
 SUBJECT.(prefix).M0_allPLD = squeeze(mean(SUBJECT.(prefix).M0ASL_allPLD(:,:,:,1,:,:),6)); % average all PLD M0 control label images, and save in SUBJECT struct
 SUBJECT.(prefix).M0 = squeeze(SUBJECT.(prefix).M0_allPLD(:,:,:,1)); % save 1st PLD M0 as true M0, and save in SUBJECT struct
-SaveDataNII(SUBJECT.(prefix).M0_allPLD, [SUBJECT.ASLdir prefix '_M0_allPLD'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save M0 image all PLDs
+if strcmp(fast, 'fast')
+    % skip saving M0 image all PLDs
+else
+    SaveDataNII(SUBJECT.(prefix).M0_allPLD, [SUBJECT.ASLdir prefix '_M0_allPLD'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save M0 image all PLDs
+end
 disp('Saving M0 image')
 SaveDataNII(SUBJECT.(prefix).M0, [SUBJECT.ASLdir prefix '_M0'], SUBJECT.dummyfilenameSaveNII, 1,[], SUBJECT.TR)  % save M0 image , only first PLDs
---
+++ ScriptStudy_ASLClinicalCVR_UMCU_MOYAMOYA_FAST.m
@@ -0,0 +1,101 @@
+
+% ClinicalASL toolbox 2023, JCWSiero
+%%%%%%%%%%%%%%%%%%%%% ASL Analysis %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% written by Jeroen Siero  25-05-2023 for the MOYAMOYA study
+% includes automatic DICOM file loading, anatomy segmentation and  registration, outlier removal, data construction, BASIL analysis, CBF, map smoothing, CVR registration, calculation and saving
+clear all
+close all
+clc
+SUBJECT.masterdir='/Fridge/users/jeroen/MOYAMOYA/';
+
+SUBJECT.tau = 2; % Label duration
+SUBJECT.N_BS = 4; % Number of background suppression pulses
+SUBJECT.labeleff = 0.85; %PCASL label efficiency
+SUBJECT.lambda = 0.9;%water partition fraction, Alsop MRM 2014
+SUBJECT.T1t = 1.3;    %s T1 of tissue DEFAULT is 1.3 at 3T, Alsop MRM 2014
+SUBJECT.T1b = 1.65;   %s T1 of arterial blood DEFAULT is 1.65 at 3T, Alsop MRM 2014
+SUBJECT.FWHM = 6; % smoothing kernel size 6mm FWHM, for CBF, AAT
+SUBJECT.FWHM_M0 = 5; % smoothing kernel size  5mm FWHM, for M0_forQCBF for manual quantification
+SUBJECT.range_adult_cbf = [0 75]; % colourbar range for adult CBF values
+SUBJECT.range_child_cbf = [0 125]; % colourbar range for child CBF values
+SUBJECT.range_cvr = [-50 50]; % colourbar range for CVR values
+SUBJECT.range_AAT = [0.5 2.5]; % time (s), arterial arrival time
+
+%% %%%%%%%%%%%%%%%%%%%%%%% 1. Subject information %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Get subject folder name, select folder containing all patient data
+
+SUBJECT.SUBJECTdir = uigetdir(SUBJECT.masterdir,'Select subject folder');
+
+% create folder paths
+SUBJECT.DICOMdir = [SUBJECT.SUBJECTdir,'/DICOM/']; % DICOM  path
+SUBJECT.NIFTIdir = [SUBJECT.SUBJECTdir,'/NIFTI/']; % NIFTI  path
+SUBJECT.ASLdir = [SUBJECT.SUBJECTdir,'/ASL/']; % ASL path
+SUBJECT.RESULTSdir = [SUBJECT.SUBJECTdir,'/ASL/FIGURE_RESULTS/']; % RESULTS path
+% extra FSL BASIL options .txt location
+SUBJECT.locationBASILinfo=[SUBJECT.masterdir 'BASIL_OPTIONS.txt']; % location .txt file with addition model options for CBF quantification BASIL
+
+% create folders
+if logical(max(~isfolder({SUBJECT.NIFTIdir; SUBJECT.ASLdir; SUBJECT.RESULTSdir})))
+    mkdir(SUBJECT.NIFTIdir); % create NIFTI folder
+    mkdir(SUBJECT.ASLdir); % create ASL folder
+    mkdir(SUBJECT.RESULTSdir); % create RESULTS folder
+end
+
+% convert and rename DICOM files in DICOM folder to NIFTI folder
+ASLConvertDICOMtoNIFTI(SUBJECT.DICOMdir, SUBJECT.NIFTIdir)
+
+% Get ASL nifti filenames
+% preACZ path
+filepreACZ = dir([SUBJECT.NIFTIdir, '*SOURCE_ASL*preACZ*2.nii.gz']);% find SOURCE data ASL
+% postACZ path
+filepostACZ = dir([SUBJECT.NIFTIdir, '*SOURCE_ASL*postACZ*2.nii.gz']);% find SOURCE data ASL
+
+if (size(filepreACZ,1) > 1 ) || (size(filepostACZ,1) > 1)
+    warning(' More than 1 preAZC or postACZ ASL dataset found -  !! taking the last scanned dataset... !!')
+end
+
+SUBJECT.preACZfilenameNIFTI = filepreACZ(end,1).name;
+SUBJECT.preACZfilenameDCM = filepreACZ(end,1).name(1:end-7);
+SUBJECT.postACZfilenameNIFTI = filepostACZ(end,1).name;
+SUBJECT.postACZfilenameDCM = filepostACZ(end,1).name(1:end-7);
+
+%% %%%%%%%%%%%%%%%%%%%%%%% 2. Extract DICOM information %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Fetch scan parameters
+SUBJECT = ASLExtractParamsDICOM(SUBJECT, SUBJECT.preACZfilenameDCM);
+SUBJECT.dummyfilenameSaveNII = [SUBJECT.NIFTIdir SUBJECT.preACZfilenameNIFTI]; % lication .nii.gz NIFTI to be used as dummy template for saving NII's in the tool
+% Obtain Look-Locker correction factor
+SUBJECT.LookLocker_correction_factor_perPLD = ASLLookLockerCorrectionFactor_mDelayPCASL(SUBJECT); % LookLocker correction factor, depending on the flipangle and PLDs
+
+%% %%%%%%%%%%%%%%%%%%%%%%% 3. Modify NIFTI to correct names, correct Mz loss (small fip angle) using Look Locker correction %%%%%%%%%%%%%%%%%%%%%
+% save per PLD and control and label volumes (interleaved), and save all ASL and M0 in struct SUBJECT
+SUBJECT = ASLPrepareASLDataDICOM(SUBJECT, SUBJECT.preACZfilenameNIFTI, 'preACZ', 'fast'); % preACZ
+SUBJECT = ASLPrepareASLDataDICOM(SUBJECT, SUBJECT.postACZfilenameNIFTI, 'postACZ', 'fast'); % postACZ
+
+disp('DICOMs converted to NIFTI');
+
+%% %%%%%%%%%%%%%%%%%%%%%%%% 4. Generate T1 from M0 , T1 Tissue segmentation and registration to T1 anatomy and MNI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% create T1fromM0 from ASL multiPLD data
+SUBJECT = ASLT1fromM0Processing(SUBJECT, 'preACZ', 'fast');
+SUBJECT = ASLT1fromM0Processing(SUBJECT, 'postACZ','fast');
+
+%% %%%%%%%%%%%%%%%%%%%%%%%% 6. BASIL CBF Analysis for both Original and Outlier removed ASL data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+disp('Perform BASIL analysis for both original and outlier removed ASL data')
+
+session = {'preACZ', 'postACZ'};
+
+for i=1:length(session)
+    prefix = char(session(i));
+    % %%%% % all PLD for AAT (arterial arrival time map) % %%%%%
+    ASLBASILanalysis(SUBJECT, [SUBJECT.ASLdir prefix '_allPLD_label1label2.nii.gz'], [SUBJECT.ASLdir prefix '_M0.nii.gz'], [SUBJECT.ASLdir prefix '_M0_brain_mask.nii.gz'], [SUBJECT.ASLdir prefix '_BASIL_allPLD_forAAT'], [1:SUBJECT.NPLDS], SUBJECT.locationBASILinfo)
+    % %%%% % 2tolast PLD for CBF map % %%%% %
+    ASLBASILanalysis(SUBJECT, [SUBJECT.ASLdir prefix '_2tolastPLD_label1label2.nii.gz'], [SUBJECT.ASLdir prefix '_M0.nii.gz'], [SUBJECT.ASLdir prefix '_M0_brain_mask.nii.gz'], [SUBJECT.ASLdir prefix '_BASIL_2tolastPLD_forCBF'], [2:SUBJECT.NPLDS], SUBJECT.locationBASILinfo)
+end
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%  7. Generate resulting CBF/CVR/AAT/aCBV .png images %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SUBJECT = ASLSaveResultsCBFAATCVR_FAST(SUBJECT); %
+
+%% %%%%%%%%%%%%%%%%%%%%%%%% 8. Save workspace %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Workspace_ClinicalASL = [SUBJECT.SUBJECTdir '/Workspace_ClinicalASL.mat'];
+save(Workspace_ClinicalASL,'-v7.3');
+disp('-- Finished --');
+
